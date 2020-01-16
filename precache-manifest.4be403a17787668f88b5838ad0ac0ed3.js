75b4201a0a021a23e521c67d782a0381110bb7bc
Deguerville Romain <roromidev@gmail.com>
2020-01-16 14:27:34 +0100

    Updates

---
+++ precache-manifest.4be403a17787668f88b5838ad0ac0ed3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279bbc842703bef8b20910957d7a6bb6",
+    "url": "/youpijob-ui-doc/index.html"
+  },
+  {
+    "revision": "0b006ad1698b9930a6b0",
+    "url": "/youpijob-ui-doc/static/css/2.f6e6f51f.chunk.css"
+  },
+  {
+    "revision": "40a4c509ab45ed6abfb1",
+    "url": "/youpijob-ui-doc/static/css/main.1870f0d0.chunk.css"
+  },
+  {
+    "revision": "0b006ad1698b9930a6b0",
+    "url": "/youpijob-ui-doc/static/js/2.89fa21e8.chunk.js"
+  },
+  {
+    "revision": "83f376f2f682405ebbf9db439c7b1a95",
+    "url": "/youpijob-ui-doc/static/js/2.89fa21e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40a4c509ab45ed6abfb1",
+    "url": "/youpijob-ui-doc/static/js/main.f6bf3f13.chunk.js"
+  },
+  {
+    "revision": "f181983918b0b63525bb",
+    "url": "/youpijob-ui-doc/static/js/runtime-main.27d038e4.js"
+  },
+  {
+    "revision": "b3d7e5e8a285e8f983a82cc1c1c0c8d2",
+    "url": "/youpijob-ui-doc/static/media/Button.b3d7e5e8.png"
+  },
+  {
+    "revision": "44d91f54a6d88ebb7d195073a05da0d9",
+    "url": "/youpijob-ui-doc/static/media/Selectable.44d91f54.png"
+  },
+  {
+    "revision": "3c23e7280037ab5c66cc14458d1565fc",
+    "url": "/youpijob-ui-doc/static/media/Text.3c23e728.png"
+  }
+]);
